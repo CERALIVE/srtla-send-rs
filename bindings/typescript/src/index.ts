aff3c087f76f6dfc51b43cfa408c98ac2842ddbc
Andres Cera <Andres.cera@hotmail.com>
2026-06-21 08:05:53 -0500

    bindings: add field docs, root-level telemetry exports and JSDoc

--- bindings/typescript/src/index.ts
+++ bindings/typescript/src/index.ts
@@ -1,2 +1,4 @@
 export * from './sender/index.js';
+export type { TelemetryUpdate } from './telemetry/index.js';
 export * from './telemetry/index.js';
+export { senderTelemetryPath } from './telemetry/index.js';
